aa32c37affdc001a889705ba8f2b8b3babbc1613
khaldoon khaldi <khaldoon9kh9@gmail.com>
2020-10-20 16:59:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+let element = document.querySelector("main#main");
+element.remove()
+
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "Khaldoon is the champion"
+document.body.appendChild(newHeader)
+
+
+
